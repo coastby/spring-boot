9270c4fa22c263102ae2a433018ae2f0da2541ce
JO YEJI <whdpwl2@gmail.com>
2022-10-26 15:26:03 +0900

    requestmapping 변경, property 삭제

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,11 +1,12 @@
 package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping(name = "/hello", method = RequestMethod.GET)
     public String hello(){
         return "Hello World";
     }
