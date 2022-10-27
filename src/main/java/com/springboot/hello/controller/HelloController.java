4dbf577b2ea6f295f34276ffc274aae67b377218
JO YEJI <whdpwl2@gmail.com>
2022-10-27 11:59:21 +0900

    @ReauestParam 실습2

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1/get-api")
 public class HelloController {
@@ -21,8 +23,17 @@ public class HelloController {
     public String getVariable2(@PathVariable("variable") String var){
         return var;
     }
+
+    //10월 27일 실습
     @GetMapping(value = "/request1")
     public String getRequestParam(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
         return String.format("%s %s %s", name, email, organization);
+    }
+    @GetMapping(value = "/request2")
+    public String getRequestParam2 (@RequestParam Map<String, String> param){
+        param.entrySet().forEach((map) -> {
+            System.out.printf("key : %s, value : %s\n", map.getKey(), map.getValue());
+        });
+        return "호출이 완료되었습니다.";
     }
 }
