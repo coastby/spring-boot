dcc255bf062e0058a0bdde0d1fc2c45e6fc4f1a8
JO YEJI <whdpwl2@gmail.com>
2022-10-27 11:26:21 +0900

    @RequestParam 실습

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -21,4 +21,8 @@ public class HelloController {
     public String getVariable2(@PathVariable("variable") String var){
         return var;
     }
+    @GetMapping(value = "/request1")
+    public String getRequestParam(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+        return String.format("%s %s %s", name, email, organization);
+    }
 }
