a8349cb5a83e11cb21fe0e9b60771d0f38599fff
JO YEJI <whdpwl2@gmail.com>
2022-10-27 14:49:54 +0900

    @RequestBody로 POST 메서드 구현 DTO 객체를 활용한 POST API 구현 ResponseEntity를 활용한 PUT 메서드 구현

--- src/main/java/com/springboot/hello/controller/PutController.java
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -2,13 +2,16 @@ package com.springboot.hello.controller;
 
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/post-api")
-public class PostController {
+@RequestMapping("api/v1/put-api")       //URI 변경
+public class PutController {
+    //////POST와 동일
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample(){
         return "Hello Post API";
@@ -25,5 +28,14 @@ public class PostController {
     @PostMapping(value = "/member2")
     public String postMemberDto(@RequestBody MemberDto memberDto) {
         return memberDto.toString();
+    }
+    /////POST와 동일
+
+    //put 추가
+    @PutMapping("/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)        //응답 코드 202, 이 메서드를 대상으로 요청을 수행하면 응답 코드가 202로 변경됨
+                .body(memberDto);
     }
 }
