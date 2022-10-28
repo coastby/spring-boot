305924ee13d92b46898014d1f65c59c641df7576
JO YEJI <whdpwl2@gmail.com>
2022-10-28 15:24:14 +0900

    UserController delete() 추가 UserDao 메서드 반환 타입 수정

--- src/main/java/com/springboot/hello/controller/UserController.java
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -3,9 +3,15 @@ package com.springboot.hello.controller;
 
 import com.springboot.hello.dao.UserDao;
 import com.springboot.hello.domain.dto.User;
+import com.springboot.hello.domain.dto.UserRequestDto;
+import io.swagger.models.auth.In;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("api/v1")
@@ -18,18 +24,30 @@ public class UserController {
         this.userDao = userDao;
     }
 
-    @PostMapping(value = "/name")
-    public String createUser(@RequestBody User user){
-        userDao.add(user);
-        String str = user.getId()+"가 등록되었습니다.";
-        log.info(str);
-        return str;
+    @PostMapping(value = "/user")
+    public ResponseEntity<Integer> createUser(@RequestBody UserRequestDto userRequestDto){
+        User user = new User(userRequestDto.getId(), userRequestDto.getName(), userRequestDto.getPassword());
+        return ResponseEntity
+                .ok()
+                .body(userDao.add(user));
+    }
+    @GetMapping(value = "/user/{id}")
+    public User getById(@PathVariable String id){
+        User user = userDao.findById(id);
+        return user;
     }
 
-    @GetMapping(value = "/user")
-    public User getById(@RequestParam)
-
-
-
+    @DeleteMapping(value = "/user/delete/all")
+    public ResponseEntity<Integer> deleteAll() throws SQLException {
+        return ResponseEntity
+                .ok()
+                .body(userDao.deleteAll());
+    }
+    @GetMapping(value = "user/delete/{id}")
+    public ResponseEntity<Integer> deleteById(@PathVariable String id){
+        return ResponseEntity
+                .ok()
+                .body(userDao.deleteById(id));
+    }
 
 }
